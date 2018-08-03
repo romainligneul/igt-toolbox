48c8ecf3e6450ca035bcc3eb83e44a87482977f5
Romain Ligneul <romlig@mentat004.dccn.nl>
2018-08-03 17:49:15 +0200

    delete

--- Tools/OTHERS/gramm-master/@gramm/no_legend.m
+++
@@ -1,8 +0,0 @@
-function obj=no_legend(obj)
-% no_legend() remove side legend on the plot
-%
-% Useful when plotting multiple gramm objects with the same
-% legend
-
-obj.with_legend=false;
-end
--- Tools/VBA/subfunctions/f_vgo_dummy.m
+++
@@ -1,3 +0,0 @@
-function [fx] = f_vgo_dummy(x,P,u,in)
-
-fx = x + sig(P(1))*(u-x) + sig(P(2))*x;
